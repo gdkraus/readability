3473be75e0287bea32b7b962fd4b3ff2d9401834
Greg Kraus <gdkraus@ncsu.edu>
2013-03-20 14:21:45 -0400

    made jQuery local to the directory

--- readability.php
+++ readability.php
@@ -4,8 +4,8 @@ header("Content-type: text/javascript");
 
 //optionally load jQuery from a local source
 
-$code = file_get_contents('../../jquery/jquery-1.6.4.min.js');
-echo $code;
+$jq = file_get_contents('jquery-1.9.1.min.js');
+echo $jq;
 
 
 $code = file_get_contents('code.js');
